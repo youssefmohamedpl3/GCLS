84c30f69199b18bdb7efbb1b8f19d54cb4dc0657
Your Name <y.newoten@gmail.com>
2025-03-24 15:52:11 +0200

    Add venv and update .gitignore

--- api/app.py
+++ api/app.py
@@ -32,11 +32,11 @@ class User(UserMixin):
 
 # Hardcoded data (replace with database in production)
 users = {
-    'admin': User('1', 'Youssef Mohamed Ahmed', 'c29FLBV593', 'all'),
-    'YMAS' : User('3',  'Youssef Mohamed Ahmed Sayed', 'c29FLBV593', 'all'),
-    'MMM' : User('2', 'Mahmoud Mohamed Mahmoud', 'R9T5B2L8', [3, 2, 6, 5]),
-    'SWA': User('4', 'Sandy Wassim Abdullah', '3K7M4N1Q', [1, 4]),
-    'KHZ': User('5','Karam Hazem Zaki', 'W6X2Y9Z4', [3, 2, 6, 5])
+    'admin': User('1', 'admin', 'c29FLBV593', 'all'),
+    'YMAS' : User('3',  'YMAS', 'c29FLBV593', 'all'),#Youssef Mohamed Ahmed
+    'MMM' : User('2', 'MMM', 'R9T5B2L8', [3, 2, 6, 5]),#Mahmoud Mohamed Mahmoud
+    'SWA': User('4', 'SWA', '3K7M4N1Q', [1, 4]),#Sandy Wassim Abdullah
+    'KHZ': User('5','KHZ', 'W6X2Y9Z4', [3, 2, 6, 5])#Karam Hazem Zaki
 }
 
 students = [
